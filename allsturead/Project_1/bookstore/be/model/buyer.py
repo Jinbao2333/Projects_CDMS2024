ef49002dd94c6a7a26232b36270786cab194ddc5
immorrrtal <2323175054@qq.com>
2024-04-24 20:47:55 +0800

    buyer receive, cancel, check

--- allsturead/Project_1/bookstore/be/model/buyer.py
+++ allsturead/Project_1/bookstore/be/model/buyer.py
@@ -114,7 +114,6 @@ class Buyer(db_conn.DBConn):
             return 528, "{}".format(str(e))
         return 200, "ok"
 
-
     def add_funds(self, user_id, password, add_value) -> (int, str):
         try:
             result = self.conn.user_col.find_one({"user_id": user_id})
@@ -130,3 +129,193 @@ class Buyer(db_conn.DBConn):
             return 528, "{}".format(str(e))
 
         return 200, ""
+    
+    def receive_books(self, user_id: str, order_id: str) -> (int, str):
+        try :
+            result = self.conn.order_col.find_one({
+                "$or": [
+                    {"order_id": order_id, "status": 1},
+                    {"order_id": order_id, "status": 2},
+                    {"order_id": order_id, "status": 3},
+                ]
+            })
+            if result == None:
+                return error.error_invalid_order_id(order_id)
+            buyer_id = result.get("user_id")
+            paid_status = result.get("status")
+
+            if buyer_id != user_id:
+                return error.error_authorization_fail()
+            if paid_status == 1:
+                return error.error_books_not_sent()
+            if paid_status == 3:
+                return error.error_books_repeat_receive()
+
+            self.conn.order_col.update_one({"order_id": order_id}, {"$set": {"status": 3}})
+        except BaseException as e:
+            return 528, "{}".format(str(e))
+        return 200, "ok"
+
+    def cancel_order(self, user_id: str, order_id: str) -> (int, str):
+        try:
+            # 未付款
+            result = self.conn.order_col.find_one({"order_id": order_id, "status": 0})
+            if result:
+                buyer_id = result.get("user_id")
+                if buyer_id != user_id:
+                    return error.error_authorization_fail()
+                store_id = result.get("store_id")
+                price = result.get("price")
+                self.conn.order_col.delete_one({"order_id": order_id, "status": 0})
+            # 已付款
+            else:
+                result = self.conn.order_col.find_one({
+                    "$or": [
+                        {"order_id": order_id, "status": 1},
+                        {"order_id": order_id, "status": 2},
+                        {"order_id": order_id, "status": 3},
+                    ]
+                })
+                if result:
+                    buyer_id = result.get("user_id")
+                    if buyer_id != user_id:
+                        return error.error_authorization_fail()
+                    store_id = result.get("store_id")
+                    price = result.get("price")
+
+                    result1 = self.conn.store_col.find_one({"store_id": store_id})
+
+                    if result1 is None:
+                        return error.error_non_exist_store_id(store_id)
+                    seller_id = result1.get("user_id")
+
+                    result2 = self.conn.user_col.update_one({"user_id": seller_id}, {"$inc": {"balance": -price}})
+                    if result2 is None:
+                        return error.error_non_exist_user_id(seller_id)
+
+
+                    result3 = self.conn.user_col.update_one({"user_id": buyer_id}, {"$inc": {"balance": price}})
+                    if result3 is None:
+                        return error.error_non_exist_user_id(user_id)
+
+                    result4 = self.conn.order_col.delete_one({
+                    "$or": [
+                        {"order_id": order_id, "status": 1},
+                        {"order_id": order_id, "status": 2},
+                        {"order_id": order_id, "status": 3},
+                    ]
+                })
+                    if result4 is None:
+                        return error.error_invalid_order_id(order_id)
+
+                else:
+                    return error.error_invalid_order_id(order_id)
+
+            # recovery the stock
+            result = self.conn.order_detail_col.find({"order_id": order_id})
+            for book in result:
+                book_id = book["book_id"]
+                count = book["count"]
+                result1 = self.conn.store_col.update_one({"store_id": store_id, "books.book_id": book_id}, {"$inc": {"books.$.stock_level": count}})
+                if result1.modified_count == 0:
+                    return error.error_stock_level_low(book_id) + (order_id,)
+
+            self.conn.order_col.insert_one({"order_id": order_id, "user_id": user_id, "store_id": store_id, "price": price, "status": 4})
+        except BaseException as e:
+            return 528, "{}".format(str(e))
+        return 200, "ok"
+
+    def check_hist_order(self, user_id: str):
+        try:
+            if not self.user_id_exist(user_id):
+                return error.error_non_exist_user_id(user_id)
+            ans = []
+            # 未付款
+            result = self.conn.order_col.find({"user_id": user_id, "status": 0})
+            if result:
+                for order in result:
+                    tmp_details = []
+                    order_id = order.get("order_id")
+                    order_detail_result = self.conn.order_detail_col.find({"order_id": order_id})
+
+                    if order_detail_result:
+                        for order_detail in order_detail_result:
+                            tmp_details.append({
+                                "book_id": order_detail.get("book_id"),
+                                "count": order_detail.get("count"),
+                                "price": order_detail.get("price")
+                            })
+                    else:
+                        return error.error_invalid_order_id(order_id)
+
+                    ans.append({
+                        "status": "unpaid",
+                        "order_id": order_id,
+                        "buyer_id": order.get("user_id"),
+                        "store_id": order.get("store_id"),
+                        "total_price": order.get("price"),
+                        "details": tmp_details
+                    })
+            # 已付款
+            books_status_list = ["unsent", "sent but not received", "received"]
+            result = self.conn.order_col.find({
+                "$or": [
+                    {"user_id": user_id, "status": 1},
+                    {"user_id": user_id, "status": 2},
+                    {"user_id": user_id, "status": 3},
+                ]
+            })
+            if result:
+                for i in result:
+                    tmp_details = []
+                    order_id = i.get("order_id")
+                    order_detail_result = self.conn.order_detail_col.find({"order_id": order_id})
+                    if order_detail_result:
+                        for order_detail in order_detail_result:
+                            tmp_details.append({
+                                "book_id": order_detail.get("book_id"),
+                                "count": order_detail.get("count"),
+                                "price": order_detail.get("price")
+                            })
+                    else:
+                        return error.error_invalid_order_id(order_id)
+                    ans.append({
+                        "order_id": order_id,
+                        "buyer_id": i.get("user_id"),
+                        "store_id": i.get("store_id"),
+                        "total_price": i.get("price"),
+                        "status": books_status_list[i.get("status") - 1],
+                        "details": tmp_details
+                    })
+            # 已取消
+            result = self.conn.order_col.find({"user_id": user_id, "status": 4})
+            if result:
+                for order_cancel in result:
+                    tmp_details = []
+                    order_id = order_cancel.get("order_id")
+                    order_detail_result = self.conn.order_detail_col.find({"order_id": order_id})
+                    if order_detail_result:
+                        for order_detail in order_detail_result:
+                            tmp_details.append({
+                                "book_id": order_detail.get("book_id"),
+                                "count": order_detail.get("count"),
+                                "price": order_detail.get("price")
+                            })
+                    else:
+                        return error.error_invalid_order_id(order_id)
+
+                    ans.append({
+                        "status": "cancelled",
+                        "order_id": order_id,
+                        "buyer_id": order_cancel.get("user_id"),
+                        "store_id": order_cancel.get("store_id"),
+                        "total_price": order_cancel.get("price"),
+                        "details": tmp_details
+                    })
+
+        except BaseException as e:
+            return 528, "{}".format(str(e))
+        if not ans:
+            return 200, "ok", "No orders found "
+        else:
+            return 200, "ok", ans
