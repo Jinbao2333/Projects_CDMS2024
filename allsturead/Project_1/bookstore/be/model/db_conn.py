e27c5425ec3ca6e467f118035a5f1e21c4df5b2c
Jinbao2333 <Jinbao2333@gmail.com>
2024-04-25 08:46:17 +0800

    Replace db_conn.py with new code

--- allsturead/Project_1/bookstore/be/model/db_conn.py
+++ allsturead/Project_1/bookstore/be/model/db_conn.py
@@ -5,33 +5,23 @@ class DBConn:
     def __init__(self):
         self.conn = store.get_db_conn()
 
-    def user_id_exist(self, user_id):
-        cursor = self.conn.execute(
-            "SELECT user_id FROM user WHERE user_id = ?;", (user_id,)
-        )
-        row = cursor.fetchone()
-        if row is None:
+    def _find_one(self, collection, query):
+        try:
+            result = self.conn[collection].find_one(query)
+            return result is not None
+        except Exception as e:
+            # throw ERROR msgs if necessary
+            print(f"Error in _find_one: {e}")
             return False
-        else:
-            return True
+
+    def user_id_exist(self, user_id):
+        query = {"user_id": user_id}
+        return self._find_one("user_col", query)
 
     def book_id_exist(self, store_id, book_id):
-        cursor = self.conn.execute(
-            "SELECT book_id FROM store WHERE store_id = ? AND book_id = ?;",
-            (store_id, book_id),
-        )
-        row = cursor.fetchone()
-        if row is None:
-            return False
-        else:
-            return True
+        query = {"store_id": store_id, "books.book_id": book_id}
+        return self._find_one("store_col", query)
 
     def store_id_exist(self, store_id):
-        cursor = self.conn.execute(
-            "SELECT store_id FROM user_store WHERE store_id = ?;", (store_id,)
-        )
-        row = cursor.fetchone()
-        if row is None:
-            return False
-        else:
-            return True
+        query = {"store_id": store_id}
+        return self._find_one("store_col", query)
