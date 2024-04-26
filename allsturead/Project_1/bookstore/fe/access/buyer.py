1e849e0177d5dc42e3011d24677483c06586a128
immorrrtal <2323175054@qq.com>
2024-04-26 12:47:50 +0800

    complement access/buyer.py

--- allsturead/Project_1/bookstore/fe/access/buyer.py
+++ allsturead/Project_1/bookstore/fe/access/buyer.py
@@ -20,7 +20,6 @@ class Buyer:
         for id_count_pair in book_id_and_count:
             books.append({"id": id_count_pair[0], "count": id_count_pair[1]})
         json = {"user_id": self.user_id, "store_id": store_id, "books": books}
-        # print(simplejson.dumps(json))
         url = urljoin(self.url_prefix, "new_order")
         headers = {"token": self.token}
         r = requests.post(url, headers=headers, json=json)
@@ -28,23 +27,65 @@ class Buyer:
         return r.status_code, response_json.get("order_id")
 
     def payment(self, order_id: str):
-        json = {
-            "user_id": self.user_id,
-            "password": self.password,
-            "order_id": order_id,
-        }
+        json = {"user_id": self.user_id, "password": self.password, "order_id": order_id, }
         url = urljoin(self.url_prefix, "payment")
         headers = {"token": self.token}
         r = requests.post(url, headers=headers, json=json)
         return r.status_code
 
     def add_funds(self, add_value: str) -> int:
-        json = {
-            "user_id": self.user_id,
-            "password": self.password,
-            "add_value": add_value,
-        }
+        json = {"user_id": self.user_id, "password": self.password, "add_value": add_value, }
         url = urljoin(self.url_prefix, "add_funds")
         headers = {"token": self.token}
         r = requests.post(url, headers=headers, json=json)
         return r.status_code
+
+# other func:
+
+    def receive_books(self, user_id: str, order_id: str) -> int:
+        json = {"user_id": user_id, "order_id": order_id}
+        url = urljoin(self.url_prefix, "receive_books")
+        headers = {"token": self.token}
+        r = requests.post(url, headers=headers, json=json)
+        return r.status_code
+
+    def cancel_order(self, user_id: str, order_id: str) -> int:
+        json = {"user_id": user_id, "order_id": order_id}
+        url = urljoin(self.url_prefix, "cancel_order")
+        headers = {"token": self.token}
+        r = requests.post(url, headers=headers, json=json)
+        return r.status_code
+
+    def auto_cancel_order(self, order_id: str) -> int:
+        json = {"order_id": order_id}
+        url = urljoin(self.url_prefix, "auto_cancel_order")
+        headers = {"token": self.token}
+        r = requests.post(url, headers=headers, json=json)
+        return r.status_code
+
+    def is_order_cancelled(self, order_id: str) -> int:
+        json = {"order_id": order_id}
+        url = urljoin(self.url_prefix, "is_order_cancelled")
+        headers = {"token": self.token}
+        r = requests.post(url, headers=headers, json=json)
+        return r.status_code
+
+    def check_hist_order(self, user_id: str) -> int:
+        json = {"user_id": user_id}
+        url = urljoin(self.url_prefix, "check_hist_order")
+        headers = {"token": self.token}
+        r = requests.post(url, headers=headers, json=json)
+        return r.status_code
+
+    def search(self, keyword, store_id=None, page=1):
+        # store
+        json = {
+            "keyword": keyword,
+            "page": page
+        }
+        if store_id:
+            json["store_id"] = store_id
+
+        url = urljoin(self.url_prefix, "search")
+        r = requests.post(url, json=json)
+        return r.content, r.status_code
